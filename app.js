c20716be8835a6f67ff9f5ec1ca423f3d4a918b9
jlam6132 <65249115+jlam6132@users.noreply.github.com>
2020-08-31 13:47:00 -0700

    used inquirer

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
-const fs = require("fs");
-const generatePage = require("./src/page-template.js");
+const inquirer = require("inquirer");
 
-const profileDataArgs = process.argv.slice(2);
+inquirer
+    .prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is your name?"
+        }
+    ])
+    .then(answers => console.log(answers));
 
-const [name, github] = profileDataArgs;
+//const fs = require("fs");
+//const generatePage = require("./src/page-template.js");
 
+//const profileDataArgs = process.argv.slice(2);
 
+//const [name, github] = profileDataArgs;
 
-fs.writeFile("./index.html", generatePage(name, github), err => {
-    if (err) throw new Error(err);
+// const pageHTML = generatePage(portfolioData);
 
-    console.log("Portfolio is complete! Check out index.html to see the output!");
-});
+//fs.writeFile("./index.html", generatePage(name, github), err => {
+//    if (err) throw new Error(err);
+
+//    console.log("Portfolio is complete! Check out index.html to see the output!");
+//});
