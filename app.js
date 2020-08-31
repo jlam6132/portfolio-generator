b0964e4991b23c5ee0689c428f3d89d80eb1570f
jlam6132 <65249115+jlam6132@users.noreply.github.com>
2020-08-31 12:08:56 -0700

    generated index.html

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-var profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require("fs");
+const generatePage = require("./src/page-template.js");
 
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-        console.log(profileDataArr[i]);
-    }
+const profileDataArgs = process.argv.slice(2);
 
-    console.log ("================");
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+const [name, github] = profileDataArgs;
 
-printProfileData(profileDataArgs);
 
+
+fs.writeFile("./index.html", generatePage(name, github), err => {
+    if (err) throw new Error(err);
+
+    console.log("Portfolio is complete! Check out index.html to see the output!");
+});
